d808916ac3c77880fe4984505d38d814383de75a
Julien-epi <julienranson77@gmail.com>
2025-07-16 12:20:39 +0200

    clean

--- fanmerch/src/main.tsx
+++ fanmerch/src/main.tsx
@@ -8,6 +8,7 @@ import { BrowserRouter } from 'react-router-dom';
 import { CartProvider } from './contexts/CartContext';
 import './index.css';
 
+
 const queryClient = new QueryClient();
 
 createRoot(document.getElementById('root')!).render(
